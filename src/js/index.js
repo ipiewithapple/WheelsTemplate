a7ddec316b01b0284e6612717bf04615d3366166
ipiewithapple <ipiewithapple@yandex.ru>
2019-10-30 16:15:46 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
+// FontAwesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+// Header menu
+
+const burgerBtn = document.querySelector('.menu');
+const headerNav = document.querySelector('.header__nav');
+const navLinks = document.querySelectorAll('.nav__link');
+
+burgerBtn.addEventListener('click', () => {
+  headerNav.classList.toggle('header__nav--opened');
+})
+
+navLinks.forEach((el) => {
+  el.addEventListener('click', () => {
+    headerNav.classList.remove('header__nav--opened');
+  })
+})
