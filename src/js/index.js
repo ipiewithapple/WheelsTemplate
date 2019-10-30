10a942124e445534d60632cf35bca40c1cb9b0ce
ipiewithapple <ipiewithapple@yandex.ru>
2019-10-30 23:10:03 +0300

    statistics section and sticky navbar

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,15 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-// Header menu
+// Variables
 
 const menuBtn = document.querySelector('.menu');
 const headerNav = document.querySelector('.header__nav');
 const navLinks = document.querySelectorAll('.nav__link');
 const burger = document.querySelector('.menu__burger');
+const navbar = document.querySelector('#nav');
+
+// Header menu
 
 menuBtn.addEventListener('click', () => {
   headerNav.classList.toggle('header__nav--opened');
@@ -22,9 +25,21 @@ navLinks.forEach((el) => {
   el.addEventListener('click', (evt) => {
     evt.preventDefault();
     const href = el.textContent.replace(' ', '-');
-    document.querySelector(`#${href}`).scrollIntoView({block: "center", behavior: "smooth"});
+    document.querySelector(`#${href}`).scrollIntoView({ block: "start", behavior: "smooth" });
     burger.classList.remove('menu__burger--close');
     headerNav.classList.remove('header__nav--opened');
   })
+})
+
+// Sticky Navbar
+
+const sticky = navbar.offsetTop;
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add('nav--sticky');
+  } else {
+    navbar.classList.remove('nav--sticky');
+  }
 })
 
